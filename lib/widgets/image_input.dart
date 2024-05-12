d6e2d87f2cd67f0b57f6283e74e266a0e8dc51ea
Shafin Al Karim <shafin.karim@hotmail.com>
2024-05-12 23:51:04 +0800

    init: flutter native photo, file path and sqflite

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key, required this.onSelectImage});
+
+  final void Function(File image) onSelectImage;
+
+  @override
+  State<ImageInput> createState() {
+    return _ImageInputState();
+  }
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final image = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if (image == null) return;
+    setState(() {
+      _selectedImage = File(image.path);
+    });
+    widget.onSelectImage(_selectedImage!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text('Take a picture'),
+    );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+        ),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: content,
+    );
+  }
+}
